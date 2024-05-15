58bbc76207bafc9acc481fbd388a6cb38678ca63
IOdevelp90 <141835220+IOdevelp90@users.noreply.github.com>
2024-05-15 08:59:06 -0300

    Delete LE04/Cenario07 directory

--- LE04/Cenario07/Programa.java
+++
@@ -1,8 +0,0 @@
-public class Programa {
-    public static void main(String[] args) {
-        InstituicaoController controller = new InstituicaoController();
-        InstituicaoView view = new InstituicaoView(controller);
-
-        
-    }
-}
