2e9144e6115766192fecc0ea3b76bd5d9abf4b1f
IOdevelp90 <141835220+IOdevelp90@users.noreply.github.com>
2024-05-15 22:29:56 -0300

    Programa.java

--- LE04/Cenario05/src/Programa.java
+++ LE04/Cenario05/src/Programa.java
@@ -1,3 +1,7 @@
+import br.edu.up.model;
+import br.edu.up.controller;
+import br.edu.up.view;
+
 public class Programa {
     public static void main(String[] args) {
         EmpresaDeEventosModel model = new EmpresaDeEventosModel();
